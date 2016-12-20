c91c3aaef9d72b24f804d7a78dbd34f6fd8561ba
cvilla100@gmail.com <cvilla100@gmail.com>
2016-12-20 13:56:45 -0800

    Add basic tagging

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220211505) do
+ActiveRecord::Schema.define(version: 20161220212615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pomodoro_tags", force: :cascade do |t|
+    t.integer  "pomodoro_id"
+    t.integer  "tag_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["pomodoro_id"], name: "index_pomodoro_tags_on_pomodoro_id", using: :btree
+    t.index ["tag_id"], name: "index_pomodoro_tags_on_tag_id", using: :btree
+  end
 
   create_table "pomodoros", force: :cascade do |t|
     t.integer  "user_id"
@@ -25,6 +34,12 @@ ActiveRecord::Schema.define(version: 20161220211505) do
     t.index ["user_id"], name: "index_pomodoros_on_user_id", using: :btree
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "name"
@@ -32,5 +47,7 @@ ActiveRecord::Schema.define(version: 20161220211505) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pomodoro_tags", "pomodoros"
+  add_foreign_key "pomodoro_tags", "tags"
   add_foreign_key "pomodoros", "users"
 end
