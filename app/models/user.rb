9ec85218410ac5eb381372a5b1633254837d8786
cvilla100@gmail.com <cvilla100@gmail.com>
2017-01-15 14:16:26 -0800

    Disable devise email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable #, :omniauthable
+          # :confirmable , :omniauthable
   include DeviseTokenAuth::Concerns::User
   has_many :pomodoros
 end
