c42d8fd5ecf62fceffcc3113ee0b4b25cc7303a5
cvilla100@gmail.com <cvilla100@gmail.com>
2017-01-15 14:58:16 -0800

    Fix typo in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
+          :recoverable, :rememberable, :trackable, :validatable#,
           # :confirmable , :omniauthable
   include DeviseTokenAuth::Concerns::User
   has_many :pomodoros
