26e3d20e5d7125ad4f1b521c05d19f3e4b60cb70
cvilla100@gmail.com <cvilla100@gmail.com>
2017-01-15 14:56:54 -0800

    Require auth to access all resources

--- spec/controllers/pomodoro_tags_controller_spec.rb
+++ spec/controllers/pomodoro_tags_controller_spec.rb
@@ -2,17 +2,27 @@ require 'rails_helper'
 
 RSpec.describe PomodoroTagsController, type: :controller do
   describe "GET #index" do
-    it "responds with status code 200" do
+    it "rejects non-authenticated users" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(401)
     end
+
+    # it "responds with status code 200" do
+    #   get :index
+    #   expect(response).to have_http_status(:success)
+    # end
   end
 
   describe "GET #show" do
-    it "responds with status code 200" do
+    it "rejects non-authenticated users" do
       pomodorotag = FactoryGirl.create(:pomodoro_tag)
       get :show, params: { id: pomodorotag.id }
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(401)
     end
+    # it "responds with status code 200" do
+    #   pomodorotag = FactoryGirl.create(:pomodoro_tag)
+    #   get :show, params: { id: pomodorotag.id }
+    #   expect(response).to have_http_status(:success)
+    # end
   end
 end
--- spec/controllers/pomodoros_controller_spec.rb
+++ spec/controllers/pomodoros_controller_spec.rb
@@ -2,17 +2,26 @@ require 'rails_helper'
 
 RSpec.describe PomodorosController, type: :controller do
   describe "GET #index" do
-    it "responds with status code 200" do
+    it "rejects non-authenticated users" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(401)
     end
+    # it "responds with status code 200" do
+    #   get :index
+    #   expect(response).to have_http_status(:success)
+    # end
   end
 
   describe "GET #show" do
-    it "responds with status code 200" do
+    it "rejects non-authenticated users" do
       pomodoro = FactoryGirl.create(:pomodoro)
       get :show, params: { id: pomodoro.id }
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(401)
     end
+    # it "responds with status code 200" do
+    #   pomodoro = FactoryGirl.create(:pomodoro)
+    #   get :show, params: { id: pomodoro.id }
+    #   expect(response).to have_http_status(:success)
+    # end
   end
 end
