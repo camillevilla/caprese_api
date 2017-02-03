72b911bf87e5724fd0812e3e735ef02eded620c0
cvilla100@gmail.com <cvilla100@gmail.com>
2017-02-03 15:55:12 -0800

    Reset Facebook key and move to .env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,        ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET'],   scope: 'email,profile'
-  provider :facebook,      ENV['1742310576088220'], ENV['42e275b53fcefa4c95a6166c5f373251']
+  provider :facebook,      ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET']
 end
